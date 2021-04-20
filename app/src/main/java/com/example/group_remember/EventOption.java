b549b852a33a166c1eb6da426fc6cb288513cb1e
chongyuh <chongyuh@vt.edu>
2021-04-20 02:53:34 -0400

    chongyu

---
+++ app/src/main/java/com/example/group_remember/EventOption.java
@@ -0,0 +1,14 @@
+package com.example.group_remember;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_option);
+    }
+}
