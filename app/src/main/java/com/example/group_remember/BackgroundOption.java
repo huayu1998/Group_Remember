e32107e6fe770322cc3720315d284e98647af146
Hanwen-Cheng <60452962+Hanwen-Cheng@users.noreply.github.com>
2021-04-20 23:50:12 -0400

    chw

--- app/src/main/java/com/example/group_remember/BackgroundOption.java
+++ app/src/main/java/com/example/group_remember/BackgroundOption.java
@@ -11,13 +11,14 @@ public class BackgroundOption extends AppCompatActivity {
 
 
     Button camera;
+
     ImageView backGround0;
     ImageView backGround1;
     ImageView backGround2;
     ImageView backGround3;
     ImageView backGround4;
     ImageView backGround5;
-    ImageView backGround6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +26,14 @@ public class BackgroundOption extends AppCompatActivity {
         setContentView(R.layout.activity_background_option);
 
         camera = (Button) findViewById(R.id.camera);
+
         backGround0 = (ImageView) findViewById(R.id.b0);
         backGround1 = (ImageView) findViewById(R.id.b1);
         backGround2 = (ImageView) findViewById(R.id.b2);
         backGround3 = (ImageView) findViewById(R.id.b3);
         backGround4 = (ImageView) findViewById(R.id.b4);
         backGround5 = (ImageView) findViewById(R.id.b5);
-        backGround6 = (ImageView) findViewById(R.id.b6);
+
 
 
 
