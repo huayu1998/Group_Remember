50d08bfeec68808e204efddd2b3a24d27ce7c986
Hanwen-Cheng <60452962+Hanwen-Cheng@users.noreply.github.com>
2021-04-20 22:20:04 -0400

    chw

--- app/src/main/java/com/example/group_remember/BackgroundOption.java
+++ app/src/main/java/com/example/group_remember/BackgroundOption.java
@@ -2,7 +2,7 @@ package com.example.group_remember;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +10,29 @@ import androidx.appcompat.app.AppCompatActivity;
 public class BackgroundOption extends AppCompatActivity {
 
 
-    Button back;
+    Button camera;
+    ImageView backGround0;
+    ImageView backGround1;
+    ImageView backGround2;
+    ImageView backGround3;
+    ImageView backGround4;
+    ImageView backGround5;
+    ImageView backGround6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_background_option);
 
-        back = (Button) findViewById(R.id.backButton);
+        camera = (Button) findViewById(R.id.camera);
+        backGround0 = (ImageView) findViewById(R.id.b0);
+        backGround1 = (ImageView) findViewById(R.id.b1);
+        backGround2 = (ImageView) findViewById(R.id.b2);
+        backGround3 = (ImageView) findViewById(R.id.b3);
+        backGround4 = (ImageView) findViewById(R.id.b4);
+        backGround5 = (ImageView) findViewById(R.id.b5);
+        backGround6 = (ImageView) findViewById(R.id.b6);
+
 
 
     }
