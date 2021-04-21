12aaaae19ca8dc104a27f6a180a134df9d7bd556
chongyuh <chongyuh@vt.edu>
2021-04-21 11:18:00 -0400

    chongyu

--- app/src/main/java/com/example/group_remember/MainActivity.java
+++ app/src/main/java/com/example/group_remember/MainActivity.java
@@ -8,13 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
 
     TextView time;
     TextView length;
     TextView name;
-    Button button;
+    FloatingActionButton button;
     Button detail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         time = (TextView)findViewById(R.id.time);
         length = (TextView)findViewById(R.id.length);
         name = (TextView)findViewById(R.id.name);
-        button = (Button)findViewById(R.id.addb);
+        button = (FloatingActionButton)findViewById(R.id.addb);
         detail = (Button)findViewById(R.id.detailb);
 
         button.setOnClickListener(new View.OnClickListener() {
