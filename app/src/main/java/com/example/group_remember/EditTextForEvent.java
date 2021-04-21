dad268e00b0ab141b37b0ac5547f81c788be0658
Hanwen-Cheng <60452962+Hanwen-Cheng@users.noreply.github.com>
2021-04-21 01:39:08 -0400

    chw

--- app/src/main/java/com/example/group_remember/EditTextForEvent.java
+++ app/src/main/java/com/example/group_remember/EditTextForEvent.java
@@ -1,4 +1,31 @@
 package com.example.group_remember;
 
-public class EditTextForEvent {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditTextForEvent extends AppCompatActivity{
+
+    Button finish;
+    TextView name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text);
+
+        finish = (Button) findViewById(R.id.finish);
+
+        name = (TextView) findViewById(R.id.eventName);
+
+    }
 }
+
+
+
+
+
+
