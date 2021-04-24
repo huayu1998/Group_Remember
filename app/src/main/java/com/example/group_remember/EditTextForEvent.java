d1a898636e3ca3b5d5337a2b52e46a45817f9ce9
huayu98 <huayu98@vt.edu>
2021-04-25 00:03:30 +0800

    intent connenction

--- app/src/main/java/com/example/group_remember/EditTextForEvent.java
+++ app/src/main/java/com/example/group_remember/EditTextForEvent.java
@@ -7,19 +7,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class EditTextForEvent extends AppCompatActivity{
 
     Button back, finish;
     TextView name;
     EditText editText;
+    boolean validTitleNDescription = false;
+
+    //接收信息
+    ArrayList<Date> dateList;
+    int number;
+    String version;
+    Date date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
+
+        //接收信息
+        Intent intent = getIntent();
+        Serializable serializable = getIntent().getSerializableExtra("dateList");//在另一个activity中用于获取对象
+        if(serializable != null) {
+            dateList = new ArrayList<Date>();
+            dateList.addAll((ArrayList<Date>) serializable);//之后将serializable对象强转使用即可
+        }
+        number = intent.getIntExtra("int",0);
+        version = intent.getStringExtra("version");
+        date = dateList.get(number);
 
         back = (Button) findViewById(R.id.back);
         finish = (Button) findViewById(R.id.finish);
@@ -29,18 +51,44 @@ public class EditTextForEvent extends AppCompatActivity{
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                Intent intent = new Intent(EditTextForEvent.this, EditEventOption.class);
+                intent.putExtra("dateList", (Serializable)dateList);
+                intent.putExtra("int",number);
+                intent.putExtra("version",version);
+                startActivity(intent);
             }
         });
 
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EditTextForEvent.this, Day.class);
-                startActivity(intent);
+
+                if (!setTopicNDescription()) {
+                    Toast.makeText(EditTextForEvent.this, "Please enter title and description.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent intent = new Intent(EditTextForEvent.this, EditEventOption.class);
+                    intent.putExtra("dateList", (Serializable)dateList);
+                    intent.putExtra("int",number);
+                    intent.putExtra("version",version);
+                    startActivity(intent);
+                }
             }
         });
 
+    }
+
+    public boolean setTopicNDescription() {
+
+        if (!name.getText().toString().isEmpty() && !editText.getText().toString().isEmpty()) {
+            date.setTopic(name.getText().toString());
+            date.setText(editText.getText().toString());
+            validTitleNDescription = true;
+        }
+        else {
+            validTitleNDescription = false;
+        }
+        return validTitleNDescription;
     }
 }
 
