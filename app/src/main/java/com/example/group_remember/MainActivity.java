75376d09ac173754304ee61982ce685d5baefc0a
chongyuh <chongyuh@vt.edu>
2021-04-25 21:06:14 -0400

    chongyu

--- app/src/main/java/com/example/group_remember/MainActivity.java
+++ app/src/main/java/com/example/group_remember/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
--- app/src/main/java/com/example/group_remember/ReaderAndWriter.java
+++ app/src/main/java/com/example/group_remember/ReaderAndWriter.java
@@ -29,47 +29,54 @@ public class ReaderAndWriter {
 
     public ArrayList<Date> read(Context context,String fileName) throws FileNotFoundException{
 
+        File file = new File(fileName);
+        if (file.exists()) {
+            try {
 
+                BufferedReader bufferedReader = null;
+                FileInputStream inputFile;
+                inputFile = context.openFileInput(fileName);
+                bufferedReader = new BufferedReader(new InputStreamReader(inputFile));
+                String line = " ";
+                int numOfLines = 0;
+                while ((line = bufferedReader.readLine()) != null) {
+                    StringBuilder content = new StringBuilder();
+                    content.append(line);
+                    String str = content.toString();
+                    String[] lineInfo = str.split("/");
+                    System.out.println(str);
+                    Date currentDate = new Date();
+                    currentDate.setYear(Integer.parseInt(lineInfo[0]));
+                    currentDate.setMonth(Integer.parseInt(lineInfo[1]));
+                    currentDate.setDay(Integer.parseInt(lineInfo[2]));
+                    currentDate.setImage(Integer.parseInt(lineInfo[3]));
+                    currentDate.setMusic(lineInfo[4]);
+                    currentDate.setTopic(lineInfo[5]);
+                    currentDate.setText(lineInfo[6]);
+                    datelist.add(currentDate);
+                    numOfLines++;
+                } // while
+                System.out.println(Integer.toString(numOfLines - 1) + " text have been loaded");
+                inputFile.close();
+            } // try
 
-        try {
-
-            BufferedReader bufferedReader= null;
-
-
-            FileInputStream inputFile;
-            inputFile = context.openFileInput(fileName);
-            bufferedReader = new BufferedReader(new InputStreamReader(inputFile));
-            String line=" ";
-            int numOfLines = 0;
-            while ((line=bufferedReader.readLine())!=null) {
-                StringBuilder content= new StringBuilder();
-                content.append(line);
-                String str = content.toString();
-                String[] lineInfo = str.split("/");
-                System.out.println(str);
-                Date currentDate = new Date();
-                currentDate.setYear(Integer.parseInt(lineInfo[0]));
-                currentDate.setMonth(Integer.parseInt(lineInfo[1]));
-                currentDate.setDay(Integer.parseInt(lineInfo[2]));
-                currentDate.setImage(Integer.parseInt(lineInfo[3]));
-                currentDate.setMusic(lineInfo[4]);
-                currentDate.setTopic(lineInfo[5]);
-                currentDate.setText(lineInfo[6]);
-                datelist.add(currentDate);
-                numOfLines++;
-            } // while
-            System.out.println(Integer.toString(numOfLines - 1) + " text have been loaded");
-            inputFile.close();
-        } // try
-
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("File " + fileName + " was not found");
-            throw new FileNotFoundException("No such file");
-            //return null;
-        } // catch
-        catch (IOException e) {
-            e.printStackTrace();
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("File " + fileName + " was not found");
+                throw new FileNotFoundException("No such file");
+                //return null;
+            } // catch
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            try {
+                datelist = new ArrayList<Date>();
+                this.write(context,fileName,datelist);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return datelist;
